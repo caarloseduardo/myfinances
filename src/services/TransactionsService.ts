df2965643938151a2ef46e5bd52afcb892535ae5
Carlos Eduardo <carloseduardo.and2@gmail.com>
2022-02-06 19:12:01 -0300

    feat: add update transaction on services

--- src/services/TransactionsService.ts
+++ src/services/TransactionsService.ts
@@ -1,6 +1,6 @@
 import { api } from './utils/api';
 
-import { transactionInterface } from '../types/transaction';
+import { TransactionInterface } from '../types/transaction';
 import delay from '../utils/delay';
 
 class TransactionsService {
@@ -10,10 +10,16 @@ class TransactionsService {
     return api.get('/transactions');
   }
 
-  async createTransaction(payload: transactionInterface) {
+  async createTransaction(data: TransactionInterface) {
     await delay(1000);
 
-    return api.post('/transactions', payload);
+    return api.post('/transactions', data);
+  }
+
+  async updateTransaction(data: TransactionInterface, id: string) {
+    await delay(1000);
+
+    return api.put(`/transactions/${id}`, data);
   }
 }
 
